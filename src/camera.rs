462b203283535f1917608de2d912d7e13cd77f96
DanikingRD <danikingrd@gmail.com>
2022-12-18 20:46:08 -0400

    bind camera eye to keyboard input

--- src/camera.rs
+++ src/camera.rs
@@ -10,6 +10,8 @@ pub struct Camera {
     pub eye: Vec3<f32>,
     pub target: Vec3<f32>,
     pub speed: f32,
+    up: Vec3<f32>,
+    forward: Vec3<f32>
 }
 impl Camera {
     /// Create a new [Camera] with the default parameters.
@@ -19,28 +21,36 @@ impl Camera {
             eye,
             target,
             speed: 0.1,
+            up: Vec3::unit_y(),
+            forward: target - eye,
         }
     }
     pub fn build_mvp(&self, width: f32, height: f32) -> Mat4<f32> {
-        let model = Mat4::rotation_3d(30.0f32.to_radians(), Vec3::unit_x());
+        let model = Mat4::translation_3d(Vec3::new(0.0, 0.0, 0.0));
         let projection: Mat4<f32> =
             Mat4::perspective_fov_lh_zo(self.fov.to_radians(), width, height, 0.1, 100.0);
-
-        let camera_z = (self.eye - self.target).normalized();
-        let camera_y: Vec3<f32> = Vec3::unit_y();
-        //   let camera_x = camera_y.cross(camera_z); // right vector
-        let view: Mat4<f32> = Mat4::look_at_lh(self.eye, self.target, camera_y);
+        let view: Mat4<f32> = Mat4::look_at_lh(self.eye, self.target, self.up);
         return projection * view * model;
     }
     pub fn on_update(&mut self, keycode: &VirtualKeyCode) {
-        let forward = (self.target - self.eye).normalized();
+        let forward = self.forward.normalized();
+        let forward_magnitude = self.forward.magnitude();
+        let right = forward.cross(self.up); // Vector pointing to the right
+        // TODO: figure out why camera is not moving correctly to the left and right
         match keycode {
             VirtualKeyCode::W | VirtualKeyCode::Up => {
                 self.eye += forward * self.speed;
-            },
+            }
             VirtualKeyCode::S | VirtualKeyCode::Down => {
                 self.eye -= forward * self.speed;
             }
+            VirtualKeyCode::D | VirtualKeyCode::Right => {
+                self.eye = self.target - (forward + right * self.speed).normalized() * forward_magnitude;
+            },
+            VirtualKeyCode::A | VirtualKeyCode::Left => {
+                self.eye = self.target - (forward - right * self.speed).normalized() * forward_magnitude;
+
+            },
             _ => (),
         }
     }
